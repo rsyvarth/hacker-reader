28f2990f6d4b84a91205f5919f20a0f8cef79b4c
Robert Syvarth <robert@duxter.com>
2015-09-24 08:02:16 -0500

    Add more tests

--- tests/karma/models/storyModel.js
+++ tests/karma/models/storyModel.js
@@ -6,26 +6,74 @@ describe('StoryModel', function () {
         var model = create(); 
         expect(model.events).to.be.an('object');
         expect(model.$q).to.be.a('function');
-        // expect(model.exampleService).to.be.an('object');
+        expect(model.hackerNewsService).to.be.a('object');
+        expect(model.embedService).to.be.a('object');
+        expect(model.readMarkerModel).to.be.a('object');
     });
 
-    // it('should have an empty data object', function () {
-    //     var model = create();
-    //     expect(model.data).to.be.an('object');
-    //     expect(Object.keys(model.data).length).to.equal(0);
-    // });
+    it('should have a loadStories method which returns a promise', function () {
+        var model = create(); 
+        sinon.stub(model.hackerNewsService, 'getTopStoryIds', function(){ 
+            var deferred = this.$q.defer();
+            deferred.resolve([1,2]);
+            return deferred.promise;
+        });
+        sinon.stub(model, '_getDetails', function(id){ model.stories.push(id); });
 
-    // it('should have a load method which returns a promise', function () {
-    //     var model = create();
-    //     expect(model.load).to.exist;
-    //     var res = model.load();
-    //     expect(res).to.exist;
-    //     expect(res.then).to.exist;
-    // });
+        var res = model.loadStories();
 
-    // it('should have a get method which returns data', function () {
-    //     var model = create();
-    //     expect(model.get).to.exist;
-    // });
+        model.hackerNewsService.getTopStoryIds.should.have.been.calledOnce;
+
+        res.then(function(){
+            expect(model.stories).to.be.deep.equal([1,2]);
+        });
+    });
+
+
+    it('should have a getStories method which returns a stories', function () {
+        var model = create(); 
+        model.stories = [1,2];
+
+        var res = model.getStories();
+
+        expect(res).to.be.deep.equal([1,2]);
+    });
+
+
+    it('should have a loadStories method which returns a promise', function () {
+        var model = create(); 
+        sinon.stub(model.readMarkerModel, 'saveId', function(){});
+        var story = {id: 2};
+        var res = model.setRead(story, true);
+
+        expect(story.read).to.equal(true);
+        model.readMarkerModel.saveId.should.have.been.calledWith(2, true);
+    });
+
+
+    it('should have a _getDetails method which returns a promise', function () {
+        var model = create(); 
+        sinon.stub(model.hackerNewsService, 'getStoryDetails', function(id){ 
+            var deferred = this.$q.defer();
+            deferred.resolve({id: id, url: 'test'});
+            return deferred.promise;
+        });
+        sinon.stub(model.embedService, 'getEmbed', function(url){ 
+            var deferred = this.$q.defer();
+            deferred.resolve({key: url});
+            return deferred.promise;
+        });
+        sinon.stub(model.readMarkerModel, 'isRead', function(id){ return false; });
+
+        var res = model._getDetails(1);
+
+
+        res.then(function(){
+            model.hackerNewsService.getStoryDetails.should.have.been.calledWith(1);
+            model.embedService.getEmbed.should.have.been.calledWith('test');
+            model.readMarkerModel.isRead.should.have.been.calledWith(1);
+            expect(model.stories).to.be.deep.equal([{id: 1, url: 'test', read: false, embed: {key: 'test'}}]);
+        });
+    });
 
 });
--- tests/karma/services/persistentStorageService.js
+++ tests/karma/services/persistentStorageService.js
@@ -1,11 +1,52 @@
-describe('EmbedService', function () {
+describe('PersistentStorageService', function () {
 
-    var ob = karmaHelpers.setup('service', 'EmbedService');
+    var ob = karmaHelpers.setup('service', 'PersistentStorageService');
 
-    it('should have all dependencies', function () {
-        var service = create(); 
-        expect(service.$http).to.be.an('function');
-        expect(service.$q).to.be.a('function');
+    var mockStorage = {
+      setItem: function() {},
+      removeItem: function() {},
+      getItem: function(key) { return '{"data":{"key":1}}'; },
+      clear: function() {}
+    };
+
+    it('should call getItem when we get', function () {
+        sinon.spy(mockStorage, 'getItem');
+        var service = new PersistentStorageService(mockStorage); 
+
+        var res = service.get('test');
+
+        mockStorage.getItem.should.have.been.calledWith('test');
+        expect(res).to.deep.equal({key:1});
+    });
+
+
+    it('should call setItem when we set', function () {
+        sinon.spy(mockStorage, 'setItem');
+        var service = new PersistentStorageService(mockStorage); 
+
+        var res = service.set('test');
+
+        mockStorage.setItem.should.have.been.calledWith('test');
+    });
+
+
+    it('should call removeItem when we remove', function () {
+        sinon.spy(mockStorage, 'removeItem');
+        var service = new PersistentStorageService(mockStorage); 
+
+        var res = service.remove('test');
+
+        mockStorage.removeItem.should.have.been.calledWith('test');
+    });
+
+
+    it('should call clear when we reset', function () {
+        sinon.spy(mockStorage, 'clear');
+        var service = new PersistentStorageService(mockStorage); 
+
+        var res = service.reset();
+
+        mockStorage.clear.should.have.been.calledOnce;
     });
 
 });
