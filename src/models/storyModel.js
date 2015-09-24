5b48507b8bbb3ebe744d7569abf2fbd368b7d951
Robert Syvarth <robert@duxter.com>
2015-09-23 23:23:46 -0500

    Forgot updates to storyModel

--- src/models/storyModel.js
+++ src/models/storyModel.js
@@ -1,6 +1,16 @@
 'use strict';
+
+/**
+ * Define the ENTRIES_LOADED event which this model emits
+ */
 namespace('models.events').ENTRIES_LOADED = 'StoryModel.ENTRIES_LOADED';
 
+/**
+ * Story Model
+ *
+ * Pulls most popular stories plus scrapes each story's target
+ * url in order to retrieve embed information.
+ */
 var StoryModel = Class.extend({
   stories: [],
   events: null,
@@ -9,6 +19,9 @@ var StoryModel = Class.extend({
   embedService: null,
   readMarkerModel: null,
 
+  /**
+   * Init class
+   */
   init: function(Events, $q, HackerNewsService, EmbedService, ReadMarkerModel) {
     this.events = Events;
     this.$q = $q;
@@ -17,18 +30,20 @@ var StoryModel = Class.extend({
     this.readMarkerModel = ReadMarkerModel;
   },
 
+  /**
+   * Load the top stories filtered by read state
+   */
   loadStories: function(page) {
     var deferred = this.$q.defer();
     var limit = 20;
     page = page > 1 ? page : 1;
 
-    this.hackerNewsService.getTopStoryIds().then(function(data) {
-      data = this.readMarkerModel.filterStoryIds(data);
-
+    this.hackerNewsService.getTopStoryIds().then(function(ids) {
       var offset = (page - 1) * limit;
-      var ids = data.slice(offset, offset + limit);
       var promises = [];
 
+      ids = this.readMarkerModel.filterStoryIds(ids);
+      ids = ids.slice(offset, offset + limit);
       this.stories = [];
 
       for (var i = 0; i < ids.length; i++) {
@@ -45,29 +60,44 @@ var StoryModel = Class.extend({
     return deferred.promise;
   },
 
+  /**
+   * Get the list of stories
+   */
   getStories: function() {
     return this.stories;
   },
 
+  /**
+   * Set a story as read/unread
+   */
   setRead: function(story, val) {
     story.read = val;
     this.readMarkerModel.saveId(story.id, val);
   },
 
+  /**
+   * Retrieve the details + embed information for a particular article
+   */
   _getDetails: function(id) {
     var deferred = this.$q.defer();
     var story = null;
 
     this.hackerNewsService.getStoryDetails(id).then(function(data) {
+
       this.stories.push(data);
       data.read = this.readMarkerModel.isRead(data.id);
       story = data;
+
     }.bind(this)).then(function() {
+
       if (!story.url) { return; }
       return this.embedService.getEmbed(story.url);
+
     }.bind(this)).then(function(embed) {
+
       story.embed = embed;
       deferred.resolve();
+
     }, function() {
       deferred.resolve();
     });
